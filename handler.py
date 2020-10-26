7874f3daf79ad99c17c98b681d96b166deeaf0cf
John Madrigal <johnmadrigal17@gmail.com>
2020-10-26 10:29:38 -0700

    run a successful api call

--- handler.py
+++ handler.py
@@ -1,8 +1,11 @@
 import json
 import datetime
+import requests
 
 
 def endpoint(event, context):
+  request = requests.get('https://www.metaweather.com/api/location/search/?query=london')
+  print(request.text)
   current_time = datetime.datetime.now().time()
   body = {
     "message": "Goodbye, the current time is " + str(current_time)
