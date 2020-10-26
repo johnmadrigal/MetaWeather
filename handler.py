f7d4f98242c70bd8a18f1694e17e7757de7e206d
John Madrigal <johnmadrigal17@gmail.com>
2020-10-26 10:59:56 -0700

    access csv locations

--- handler.py
+++ handler.py
@@ -1,11 +1,22 @@
 import json
 import datetime
 import requests
+import csv
 
 
 def endpoint(event, context):
-  request = requests.get('https://www.metaweather.com/api/location/search/?query=london')
-  print(request.text)
+  csvfile = open('locations.csv')
+  location_reader = csv.reader(csvfile, delimiter=',')
+  dict_reader = csv.DictReader(csvfile)
+  for row in dict_reader:
+    print('Location:', row['Location'])
+  # for row in location_reader:
+  #   print('row', row[0])
+  # print('read', read)
+  # print('event', event)
+  # print('context', context)
+  # request = requests.get('https://www.metaweather.com/api/location/search/?query=london')
+  # print(request.text)
   current_time = datetime.datetime.now().time()
   body = {
     "message": "Goodbye, the current time is " + str(current_time)
