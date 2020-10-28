0f40bb3a35f7647665d439a9bc72f01b58890699
John Madrigal <johnmadrigal17@gmail.com>
2020-10-27 21:59:17 -0700

    cleanup

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>MetaWeather API with Python and serverless framework</h1>
+
--- handler.py
+++ handler.py
@@ -1,44 +1,45 @@
 import json
-import csv
-import boto3
 import asyncio
 import aiohttp
 from utils import fetch, get_csv_column, get_s3_file
 from weather import Forecast
 
+BUCKET_NAME = 'primelocations'
+OBJECT_NAME = 'locations.csv'
 
 def endpoint(event, context):
     # get file from s3
-    csv_file = get_s3_file('primelocations', 'locations.csv')
+    csv_file = get_s3_file(BUCKET_NAME, OBJECT_NAME)
 
     # get locations from csv
     locations = get_csv_column(csv_file, 'Location')
 
-    # develop appropriate query string locations
-    queries = []
+    # query api based on locations
+    location_queries = []
     for location in locations:
         query_string = location.replace(' ', '%20')
-        query_location = f'https://www.metaweather.com/api/location/search/?query={query_string}'
-        queries.append(query_location)
+        location_query = f'https://www.metaweather.com/api/location/search/?query={query_string}'
+        location_queries.append(location_query)
 
-    async def gather(future):
+    # main section for api calls to get 5 day forecast for locations
+    async def main(future):
         async with aiohttp.ClientSession() as session:
 
             # get location data from api
-            fetch_locations = []
-            for query in queries:
-                fetch_locations.append(fetch(session, query))
+            fetch_locations = [fetch(session, url) for url in location_queries]
             location_data = await asyncio.gather(*fetch_locations)
 
             # get weather data based of woeids
-            weathers = []
+            weather_queries = []
             for row in location_data:
                 woeid = str(row[0]['woeid'])
-                query_weather = f'https://www.metaweather.com/api/location/{woeid}/'
-                weathers.append(fetch(session, query_weather))
-            weather_data = await asyncio.gather(*weathers)
+                weather_query = f'https://www.metaweather.com/api/location/{woeid}/'
+                weather_queries.append(weather_query)
 
-            # create json object from weather data
+            fetch_weathers = [fetch(session, url) for url in weather_queries]
+            weather_data = await asyncio.gather(*fetch_weathers)
+
+            # create json object from locations and weather data
             results = {}
             for location, city in zip(locations, weather_data):
                 results[location] = []
@@ -49,12 +50,15 @@ def endpoint(event, context):
                     description = day['weather_state_name']
                     forecast = Forecast(date, temp, description)
                     results[location].append(forecast)
-        return results
 
+            return results
+
+    # set and run event loop to complete async calls
+    # store final result in body
     asyncio.set_event_loop(asyncio.new_event_loop())
     loop = asyncio.get_event_loop()
     future = asyncio.Future()
-    task = asyncio.ensure_future(gather(future))
+    task = asyncio.ensure_future(main(future))
     body = loop.run_until_complete(task)
     loop.close()
 
@@ -63,3 +67,4 @@ def endpoint(event, context):
         "body": json.dumps(body)
     }
     return response
+
